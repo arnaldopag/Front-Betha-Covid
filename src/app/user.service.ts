3e0fef936e091b6da48c444d5b043a1f457e9f5e
bernardorz <bernardorizzatti01@gmail.com>
2021-12-14 12:55:15 -0300

    add header in user.service.ts

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -6,29 +6,68 @@ import {User} from './user/user'
     providedIn : 'root'
 })
 export class UserService{
-    constructor(private http : HttpClient){}
+
+
+
+
+    constructor(private http : HttpClient){
+
+
+
+
+
+    }
     
     save(user : User): Observable<User> {
        
-        return this.http.post<User>('http://localhost:8080',user)
+        return this.http.post<User>('http://localhost:8080/users',{...user, 
+        roles : [{ id : 2}]
+        }, {
+            headers : {
+                'Authorization' : "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOiJhZG1pbiIsImV4cCI6MTYzOTUyNjIzNH0.4GwC8cWuD9X7rVeoQFbNmfCKKWO7HjEJ_rjNHKJM6YA",
+            }
+        })
+    }
+
+    getUser() : Observable<User[]> {
+
+
+        const reqHeader = new HttpHeaders(
+            { 
+                'Content-Type': 'application/json',
+                'No-Auth':'True',
+                'Access-Control-Allow-Origin' : '*',
+                'Access-Control-Allow-Header':'Access-Control-Allow-Origin,X-Requested-With,Content-Type,Access',
+                'Access-Control-Allow-Methods':'GET,POST,DELETE,PATCH,PUT,OPTIONS'
+             }
+             );
+
+
+        return this.http.get<User[]>("http://localhost:8080/users",{
+            headers : {
+                'Authorization' : "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOiJhZG1pbiIsImV4cCI6MTYzOTUyNjIzNH0.4GwC8cWuD9X7rVeoQFbNmfCKKWO7HjEJ_rjNHKJM6YA",
+            }
+        })
     }
   
     getUserById(codigoDoId: number) : Observable<User>{
-        const header = new Headers()
-        header.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOiJhZG1pbiIsImV4cCI6MTYzOTUyNjIzNH0.4GwC8cWuD9X7rVeoQFbNmfCKKWO7HjEJ_rjNHKJM6YA');
-        return this.http.get<User>(`http://localhost:8080/users/1`,user,{headers : header});
+        return this.http.get<User>(`http://localhost:8080/users/1`);
     }
   
     update(user: User): Observable<any>{
-        const header = new Headers()
-        header.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOiJhZG1pbiIsImV4cCI6MTYzOTUyNjIzNH0.4GwC8cWuD9X7rVeoQFbNmfCKKWO7HjEJ_rjNHKJM6YA');
-        return this.http.put<User>(`http://localhost:8080/users/${user.id}`, user,{headers: header});
+        return this.http.put<User>(`http://localhost:8080/users/${user.id}`, user,{
+            headers : {
+                'Authorization' : "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOiJhZG1pbiIsImV4cCI6MTYzOTUyNjIzNH0.4GwC8cWuD9X7rVeoQFbNmfCKKWO7HjEJ_rjNHKJM6YA"
+            }
+        });
     }  
   
     delete(user: User): Observable<any>{
-        const header = new Headers()
-        header.set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOiJhZG1pbiIsImV4cCI6MTYzOTUyNjIzNH0.4GwC8cWuD9X7rVeoQFbNmfCKKWO7HjEJ_rjNHKJM6YA');
-      return this.http.delete<any>(`http://localhost:8080/users/${user.id}`,{headers: header});
+        return this.http.delete<any>(`http://localhost:8080/users/${user.id}`,{
+            headers : {
+                'Authorization' : "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZXMiOiJhZG1pbiIsImV4cCI6MTYzOTUyNjIzNH0.4GwC8cWuD9X7rVeoQFbNmfCKKWO7HjEJ_rjNHKJM6YA"
+            }
+      });
   }   
 
 
